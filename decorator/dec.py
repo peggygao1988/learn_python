4ba9cff64a7e8a10c3f7dfbec235c3333d00a314
peggygao <peggygao1118@gmail.com>
2015-11-16 16:46:26 +0800

    dec and oo

--- decorator/dec.py
+++ decorator/dec.py
@@ -1,23 +1,36 @@
 from functools import wraps
 
+
 def hello(func):
+    d = {}
     @wraps(func)
-    def  wrapper(*args, **kw):
+    def wrapper(*args, **kw):
+        key = args[0]
+        if key not in d:
+            d[key] = key
+            print 'first'
+        print d
         print 'hello, %s !' % (func.__name__)
         return func(*args, **kw)
         print 'goodbye, %s !' % (func.__name__)
     return wrapper
 
 # without args function style
+
+
 @hello
-def foo():
+def foo(*args):
     print "I'm foo"
 
+@hello
+def foo2(*args):
+    print "I'm foo2"
 
 
 def makeHTMLTag(tag, *args, **kw):
     def real_dec(func):
         css_class = "class = {0}".format(kw.get('css_class', ''))
+
         def wrapper(*args, **kw):
             text = func(*args, **kw)
             return "<" + tag + css_class + ">" + text + "</" + tag + ">"
@@ -25,33 +38,44 @@ def makeHTMLTag(tag, *args, **kw):
     return real_dec
 
 # with args function style
+
+
 @makeHTMLTag(tag='b', css_class='bold_css')
 @makeHTMLTag(tag='i', css_class='italic_css')
 def text(name):
     return 'hello, %s' % (name)
 
 
-
-#without args class style
+# without args class style
 class MyDec(object):
+
     def __init__(self, fn):
         print 'inside MyDec __init__'
+        self.d = {}
         self.fn = fn
 
     def __call__(self, *args, **kwargs):
+        key = args[0]
+        if key not in self.d:
+            self.d[key] = key
+            print 'first'
+        print self.d
         return self.fn(*args, **kwargs)
+
 
 @MyDec
 def func(name):
     return 'goodbye, %s' % (name)
 
-
+@MyDec
+def func2(name):
+    return 'goodbye2, %s' % (name)
 # with args class style
 class makeHTMLTagClass(object):
+
     def __init__(self, tag, css_class=''):
         self._tag = tag
         self._css_class = css_class
-
 
     def __call__(self, fn):
         def wrapper(*args, **kwargs):
@@ -59,14 +83,15 @@ class makeHTMLTagClass(object):
             return '<' + self._tag + self._css_class + '>' + text + '</' + self._tag + '>'
         return wrapper
 
+
 @makeHTMLTagClass(tag="b", css_class="bold_css")
 @makeHTMLTagClass(tag="i", css_class="italic_css")
 def welcome(name):
     return "Welcome, {}".format(name)
 
 if __name__ == '__main__':
-    #foo()
-    #print text.__name__
-    #print text('joe')
-    #print func('dd')
+    # foo()
+    # print text.__name__
+    # print text('joe')
+    # print func('dd')
     print welcome('yj')
